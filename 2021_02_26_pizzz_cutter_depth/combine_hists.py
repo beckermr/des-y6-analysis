9882a739f70b0b5648c2f32bbc869fb20ad7c69c
Matthew R. Becker <becker.mr@gmail.com>
2021-02-26 16:07:10 -0600

    ENH scrip to combine the data

---
+++ 2021_02_26_pizzz_cutter_depth/combine_hists.py
@@ -0,0 +1,16 @@
+import glob
+import fitsio
+import tqdm
+
+import esutil as eu
+
+
+fnames = glob.glob("./hdata/*.fits")
+
+d = []
+for fname in tqdm(fnames):
+    d.append(fitsio.read(fname))
+
+dtot = eu.numpy_util.combine_arrlist(d)
+
+fitsio.write("hdata.fits", dtot, clobber=True)
