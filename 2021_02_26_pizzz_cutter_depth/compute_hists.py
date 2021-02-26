a66c487a0a0ca864b87900c37b7f5eb5f013c35e
Matthew R. Becker <becker.mr@gmail.com>
2021-02-26 15:25:25 -0600

    try this

--- 2021_02_26_pizzz_cutter_depth/compute_hists.py
+++ 2021_02_26_pizzz_cutter_depth/compute_hists.py
@@ -84,25 +84,25 @@ for i, tile in enumerate(tiles):
     for band in BANDS:
         jobs.append(joblib.delayed(_compute_hist_for_tile_band)(tile, band))
 
-        with joblib.Parallel(n_jobs=5, backend='loky', verbose=100, timeout=240) as para:
-            outputs = para(jobs)
-        print("done with data processing", flush=True)
+    with joblib.Parallel(n_jobs=5, backend='loky', verbose=100, timeout=240) as para:
+        outputs = para(jobs)
+    print("done with data processing", flush=True)
 
-        jobs = []
+    jobs = []
 
-        outputs = [o for o in outputs if o is not None]
+    outputs = [o for o in outputs if o is not None]
 
-        if len(outputs) > 0:
-            d = np.zeros(len(outputs), dtype=dtype)
-            for i, res in enumerate(outputs):
-                d["band"][i] = res[4]
-                d["tilename"][i] = res[3]
-                d["bin"][i] = BCEN
-                d["pizza"][i] = res[0]
-                d["stamp"][i] = res[1]
-                d["diff"][i] = res[2]
+    if len(outputs) > 0:
+        d = np.zeros(len(outputs), dtype=dtype)
+        for i, res in enumerate(outputs):
+            d["band"][i] = res[4]
+            d["tilename"][i] = res[3]
+            d["bin"][i] = BCEN
+            d["pizza"][i] = res[0]
+            d["stamp"][i] = res[1]
+            d["diff"][i] = res[2]
 
-            totd.append(d)
+        totd.append(d)
 
-            print("writing data", flush=True)
-            fitsio.write("test.fits", esutil.numpy_util.combine_arrlist(totd), clobber=True)
+        print("writing data", flush=True)
+        fitsio.write("test.fits", esutil.numpy_util.combine_arrlist(totd), clobber=True)
