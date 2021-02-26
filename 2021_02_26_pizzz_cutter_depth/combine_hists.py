e586e4ce85a6f163e032b429ab16d73a2ce04973
Matthew R. Becker <becker.mr@gmail.com>
2021-02-26 16:09:52 -0600

    again

--- 2021_02_26_pizzz_cutter_depth/combine_hists.py
+++ 2021_02_26_pizzz_cutter_depth/combine_hists.py
@@ -8,7 +8,7 @@ import esutil as eu
 fnames = glob.glob("./hdata/*.fits")
 
 d = []
-for fname in tqdm(fnames):
+for fname in tqdm.tqdm(fnames):
     d.append(fitsio.read(fname))
 
 dtot = eu.numpy_util.combine_arrlist(d)
