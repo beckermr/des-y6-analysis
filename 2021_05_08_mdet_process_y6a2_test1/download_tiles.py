ce967793c17856f667067329c47a769f22f33eac
Matthew R. Becker <becker.mr@gmail.com>
2021-05-08 08:43:05 -0500

    try this

--- 2021_05_08_mdet_process_y6a2_test1/download_tiles.py
+++ 2021_05_08_mdet_process_y6a2_test1/download_tiles.py
@@ -5,7 +5,7 @@ with open("tiles.txt", "r") as fp:
         tname = line.strip()
         subprocess.run(
             "rsync -raP --password-file ${DES_RSYNC_PASSFILE} "
-            "${DESREMOTE_RSYNC_USER}@${DESREMOTE_RSYNC}:/ACT/multiepoch/Y6A2_PIZZACUTTER/r5227/%s "
+            "${DESREMOTE_RSYNC_USER}@${DESREMOTE_RSYNC}/ACT/multiepoch/Y6A2_PIZZACUTTER/r5227/%s "
             "${DESDATA}/ACT/multiepoch/Y6A2_PIZZACUTTER/r5227/" % tname,
             shell=True,
             check=True,
