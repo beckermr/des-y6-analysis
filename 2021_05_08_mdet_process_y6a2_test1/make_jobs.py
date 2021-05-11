cece6861f3893fd7dae59386f5592ca3350a73f5
Matthew R. Becker <becker.mr@gmail.com>
2021-05-11 15:34:16 -0500

    write the script

--- 2021_05_08_mdet_process_y6a2_test1/make_jobs.py
+++ 2021_05_08_mdet_process_y6a2_test1/make_jobs.py
@@ -1,13 +1,17 @@
 import os
 import glob
+import numpy as np
 
 with open("tiles.txt", "r") as fp:
     tnames = fp.readlines()
     tnames = [t.strip() for t in tnames]
 
+rng = np.random.RandomState(seed=7)
+seeds = rng.randint(1, 2**30-1, size=len(tnames))
+
 bands = ["r", "i", "z"]
 os.makedirs("./jobs", exist_ok=True)
-for tname in tnames:
+for seed, tname in zip(seeds, tnames):
     print("making job for tile %s" % tname, flush=True)
 
     # get the meds files in riz order
@@ -28,6 +32,30 @@ for tname in tnames:
         all_found = all_found and found
     if not all_found:
         print("did not find all files for tile %s - skipping!" % tname, flush=True)
+    else:
+        for band, mfile in zip(bands, mfiles):
+            print("    %s: %s" % (band, mfile), flush=True)
 
     # build wa script and job
-    
+    with open("./jobs/wq_%s.sub" % tname, "w") as fp:
+        fp.write(f"""\
+# one whole node
+N: 1
+mode: by_node
+job_name: '{tname}'
+command: |
+  source ~/.bashrc
+  conda activate des-y6a2-test1
+  cd ~/workarea/des-y6-analysis/2021_05_08_mdet_process_y6a2_test1/outputs
+  mkdir -p /data/beckermr-mdet-y6a2-test1/{tname}
+  run-metadetect-on-slices \
+    --config=../metadetect-v3.yaml \
+    --output-path=. \
+    --use-tmpdir \
+    --tmpdir=/data/beckermr-mdet-y6a2-test1/{tname} \
+    --seed={seed} \
+    --log-level=WARNING \
+    --n-jobs=12 \
+    {mfiles[0]} {mfiles[1]} {mfiles[2]} || :
+  rm -rf /data/beckermr-mdet-y6a2-test1/{tname}
+""")
