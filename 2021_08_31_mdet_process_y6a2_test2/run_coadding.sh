d724f0cdb872b8a8d8cf1c0bbf327b1d9bb84264
Matthew R. Becker <becker.mr@gmail.com>
2021-08-31 16:33:32 -0500

    ENH try again

--- 2021_08_31_mdet_process_y6a2_test2/run_coadding.sh
+++ 2021_08_31_mdet_process_y6a2_test2/run_coadding.sh
@@ -5,6 +5,7 @@ des-pizza-cutter \
   --info=${MEDS_DIR}/des-pizza-slices-y6-v9/pizza_cutter_info/DES2132-5748_r_pizza_cutter_info.yaml \
   --output-path=`pwd` \
   --seed=3232 \
+  --log-level=INFO \
   --use-tmpdir \
   --tmpdir=/data/beckermr/tmp \
   --n-jobs=4
