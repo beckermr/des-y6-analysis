d06f14f8a73b915aacc2d09df111d73e6dd57a67
Matthew R. Becker <becker.mr@gmail.com>
2021-09-01 11:13:28 -0500

    BUG more bugs

--- 2021_08_31_mdet_process_y6a2_test2/run_mdet.sh
+++ 2021_08_31_mdet_process_y6a2_test2/run_mdet.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-run-metadetect-on-slices \
+python -m pdb `which run-metadetect-on-slices` \
   --config=metadetect-v4.yaml \
   --output-path=. \
   --seed=23487956 \
   --log-level=WARNING \
   --n-jobs=$1 \
+  --range="0:$1" \
   DES2132-5748_r_des-pizza-slices-y6-v9_meds-pizza-slices.fits.fz
