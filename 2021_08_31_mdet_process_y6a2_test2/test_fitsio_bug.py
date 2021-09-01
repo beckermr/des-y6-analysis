033e5763feba82157591e6e03455485f59179586
Matthew R. Becker <becker.mr@gmail.com>
2021-09-01 05:44:51 -0500

    ENH test script

---
+++ 2021_08_31_mdet_process_y6a2_test2/test_fitsio_bug.py
@@ -0,0 +1,70 @@
+import numpy as np
+import esutil as eu
+import scipy
+import ngmix
+import meds
+import piff
+import fitsio
+import desmeds
+import pixmappy
+from meds.maker import MEDS_FMT_VERSION
+from pizza_cutter import __version__
+import os
+
+MAGZP_REF = 30
+
+
+def _build_metadata(*, config, json_info):
+    numpy_version = np.__version__
+    scipy_version = scipy.__version__
+    esutil_version = eu.__version__
+    fitsio_version = fitsio.__version__
+    meds_version = meds.__version__
+    piff_version = piff.__version__
+    pixmappy_version = pixmappy.__version__
+    desmeds_version = desmeds.__version__
+    ngmix_version = ngmix.__version__
+    dt = [
+        ('magzp_ref', 'f8'),
+        ('config', 'S%d' % len(config)),
+        ('tile_info', 'S%d' % len(json_info)),
+        ('pizza_cutter_version', 'S%d' % len(__version__)),
+        ('numpy_version', 'S%d' % len(numpy_version)),
+        ('scipy_version', 'S%d' % len(scipy_version)),
+        ('esutil_version', 'S%d' % len(esutil_version)),
+        ('ngmix_version', 'S%d' % len(ngmix_version)),
+        ('fitsio_version', 'S%d' % len(fitsio_version)),
+        ('piff_version', 'S%d' % len(piff_version)),
+        ('pixmappy_version', 'S%d' % len(pixmappy_version)),
+        ('desmeds_version', 'S%d' % len(desmeds_version)),
+        ('meds_version', 'S%d' % len(meds_version)),
+        ('meds_fmt_version', 'S%d' % len(MEDS_FMT_VERSION)),
+        ('meds_dir', 'S%d' % len(os.environ['MEDS_DIR'])),
+        ('piff_data_dir', 'S%d' % len(os.environ.get('PIFF_DATA_DIR', ' '))),
+        ('desdata', 'S%d' % len(os.environ.get('DESDATA', ' ')))]
+    metadata = np.zeros(1, dt)
+    metadata['magzp_ref'] = MAGZP_REF
+    metadata['config'] = config
+    metadata['numpy_version'] = numpy_version
+    metadata['scipy_version'] = scipy_version
+    metadata['esutil_version'] = esutil_version
+    metadata['ngmix_version'] = ngmix_version
+    metadata['fitsio_version'] = fitsio_version
+    metadata['piff_version'] = piff_version
+    metadata['pixmappy_version'] = pixmappy_version
+    metadata['desmeds_version'] = desmeds_version
+    metadata['meds_version'] = meds_version
+    metadata['meds_fmt_version'] = MEDS_FMT_VERSION
+    metadata['pizza_cutter_version'] = __version__
+    metadata['meds_dir'] = os.environ['MEDS_DIR']
+    metadata['piff_data_dir'] = os.environ.get('PIFF_DATA_DIR', '')
+    metadata['desdata'] = os.environ.get('DESDATA', '')
+    metadata['tile_info'] = json_info
+    return metadata
+
+
+md = _build_metadata(config="", json_info="")
+
+
+with fitsio.FITS("test.fits", clobber=True) as fits:
+    fits.write(md, extname="metadata")
