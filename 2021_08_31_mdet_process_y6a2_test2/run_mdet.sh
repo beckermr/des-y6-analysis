86eae00f42408153ea4187501343a5fc7a6fc988
Matthew R. Becker <becker.mr@gmail.com>
2021-09-01 09:02:02 -0500

    ENH run mdet

---
+++ 2021_08_31_mdet_process_y6a2_test2/run_mdet.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+run-metadetect-on-slices \
+  --config=metadetect-v4.yaml \
+  --output-path=. \
+  --seed=23487956 \
+  --log-level=WARNING \
+  --n-jobs=$1 \
+  DES2132-5748_r_des-pizza-slices-y6-v9_meds-pizza-slices.fits.fz
