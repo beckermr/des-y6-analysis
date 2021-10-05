f916e6a1588e66203718f41712d015cecb78b50e
Matthew R. Becker <becker.mr@gmail.com>
2021-10-05 06:55:18 -0500

    use tmp dir

--- 2021_10_04_slice_masking/run_coadding.sh
+++ 2021_10_04_slice_masking/run_coadding.sh
@@ -6,8 +6,8 @@ for band in r i z; do
     --info=${MEDS_DIR}/des-pizza-slices-y6-v9/pizza_cutter_info/DES2229-3957_${band}_pizza_cutter_info.yaml \
     --output-path=`pwd` \
     --seed=3232 \
-    --n-jobs=4 &
+    --use-tmpdir \
+    --tmpdir=/data/beckermr/tmp \
+    --n-jobs=2 &
 done
   # --range="706:710" \
-  # --use-tmpdir \
-  # --tmpdir=/data/beckermr/tmp \
