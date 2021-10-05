be370aec731cb8190cce8b2a5061aa0909f8ea51
Matthew R. Becker <becker.mr@gmail.com>
2021-10-05 12:06:20 -0500

    ENH add code

--- 2021_10_04_slice_masking/run_coadding.sh
+++ 2021_10_04_slice_masking/run_coadding.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 for band in r i z; do
+  mkdir -p /data/beckermr/tmp/${band}
   des-pizza-cutter \
     --config des-pizza-slices-y6-v9.yaml \
     --info=${MEDS_DIR}/des-pizza-slices-y6-v9/pizza_cutter_info/DES2229-3957_${band}_pizza_cutter_info.yaml \
     --output-path=`pwd` \
     --seed=3232 \
     --use-tmpdir \
-    --tmpdir=/data/beckermr/tmp \
-    --n-jobs=2 &
+    --tmpdir=/data/beckermr/tmp/${band} \
+    --n-jobs=3 &
 done
   # --range="706:710" \
--- 2021_10_04_slice_masking/run_downloading.sh
+++ 2021_10_04_slice_masking/run_downloading.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for band in r i z; do
+for band in g r i z; do
   des-pizza-cutter-prep-tile \
     --config des-pizza-slices-y6-v9.yaml \
     --tilename DES2229-3957 \
