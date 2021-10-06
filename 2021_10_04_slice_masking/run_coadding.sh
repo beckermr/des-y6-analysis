efe63a879117eda67d15a28ebc09774436c2f9e1
Matthew R. Becker <becker.mr@gmail.com>
2021-10-06 08:50:55 -0500

    try smaller slices

--- 2021_10_04_slice_masking/run_coadding.sh
+++ 2021_10_04_slice_masking/run_coadding.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-for band in r i z; do
+for band in r; do
   mkdir -p /data/beckermr/tmp/${band}
   des-pizza-cutter \
-    --config des-pizza-slices-y6-v9.yaml \
+    --config des-pizza-slices-y6-v9-small.yaml \
     --info=${MEDS_DIR}/des-pizza-slices-y6-v9/pizza_cutter_info/DES2229-3957_${band}_pizza_cutter_info.yaml \
     --output-path=`pwd` \
     --seed=3232 \
