1d61e9648a31cbafe9f05b0add0d2fb2f11b0cc3
Matthew R. Becker <becker.mr@gmail.com>
2021-10-21 14:49:28 -0500

    do the thing

--- 2021_10_21_test_newest_code/run_img.sh
+++ 2021_10_21_test_newest_code/run_img.sh
@@ -7,8 +7,8 @@ done
 
 des-make-image-fromfiles \
     coadd-riz.jpg \
-    DES2041-5248_g_des-pizza-slices-y6-test_meds-pizza-slices-range0000-0200-coadd-img.fits \
-    DES2041-5248_r_des-pizza-slices-y6-test_meds-pizza-slices-range0000-0200-coadd-img.fits \
-    DES2041-5248_i_des-pizza-slices-y6-test_meds-pizza-slices-range0000-0200-coadd-img.fits
+    DES2041-5248_g_des-pizza-slices-y6-test_meds-pizza-slices-range0000-0200-coadd-img.fits.fz \
+    DES2041-5248_r_des-pizza-slices-y6-test_meds-pizza-slices-range0000-0200-coadd-img.fits.fz \
+    DES2041-5248_i_des-pizza-slices-y6-test_meds-pizza-slices-range0000-0200-coadd-img.fits.fz
     # --absscale 0.015 \
     # --scales 1.0,1.0,1.3
