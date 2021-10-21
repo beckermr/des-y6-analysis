adfe84910765c763f751cb9d87ca4f7590629dca
Matthew R. Becker <becker.mr@gmail.com>
2021-10-21 12:49:32 -0500

    wrong config

--- 2021_10_21_test_newest_code/run_coadding.sh
+++ 2021_10_21_test_newest_code/run_coadding.sh
@@ -3,7 +3,7 @@
 for band in g r i z; do
   mkdir -p /data/beckermr/tmp/${band}
   des-pizza-cutter \
-    --config des-pizza-slices-y6-v9-small.yaml \
+    --config des-pizza-slices-y6-v9.yaml \
     --info=${MEDS_DIR}/des-pizza-slices-y6-v9/pizza_cutter_info/DES2229-3957_${band}_pizza_cutter_info.yaml \
     --output-path=`pwd` \
     --seed=3232 \
