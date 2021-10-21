e4feead78bbf5c885d9fc7716fc0111b2d9f35e7
Matthew R. Becker <becker.mr@gmail.com>
2021-10-21 14:42:45 -0500

    and the color one

--- 2021_10_21_test_newest_code/run_img.sh
+++ 2021_10_21_test_newest_code/run_img.sh
@@ -5,10 +5,10 @@ for band in g r i z; do
     DES2041-5248_${band}_des-pizza-slices-y6-test_meds-pizza-slices-range0000-0200.fits.fz
 done
 
-# des-make-image-fromfiles \
-#     coadd-riz.jpg \
-#     DES2041-5248_g_des-pizza-slices-y6-test_meds-pizza-slices-range0000-0200.fits.fz \
-#     DES2041-5248_r_des-pizza-slices-y6-test_meds-pizza-slices-range0000-0200.fits.fz \
-#     DES2041-5248_i_des-pizza-slices-y6-test_meds-pizza-slices-range0000-0200.fits.fz
-#     # --absscale 0.015 \
-#     # --scales 1.0,1.0,1.3
+des-make-image-fromfiles \
+    coadd-riz.jpg \
+    DES2041-5248_g_des-pizza-slices-y6-test_meds-pizza-slices-range0000-0200-coadd-img.fits \
+    DES2041-5248_r_des-pizza-slices-y6-test_meds-pizza-slices-range0000-0200-coadd-img.fits \
+    DES2041-5248_i_des-pizza-slices-y6-test_meds-pizza-slices-range0000-0200-coadd-img.fits
+    # --absscale 0.015 \
+    # --scales 1.0,1.0,1.3
