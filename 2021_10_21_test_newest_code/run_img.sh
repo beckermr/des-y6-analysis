7b799867480b2ddc90108bb2b910f0d2abde6489
Matthew R. Becker <becker.mr@gmail.com>
2021-10-21 14:38:06 -0500

    bug in script

--- 2021_10_21_test_newest_code/run_img.sh
+++ 2021_10_21_test_newest_code/run_img.sh
@@ -2,8 +2,8 @@
 
 des-make-image-fromfiles \
     coadd-riz.jpg \
-    DES2041-5248_g_des-pizza-slices-y6-test_meds-pizza-slices-coadd-img.fits.fz \
-    DES2041-5248_r_des-pizza-slices-y6-test_meds-pizza-slices-coadd-img.fits.fz \
-    DES2041-5248_i_des-pizza-slices-y6-test_meds-pizza-slices-coadd-img.fits.fz
+    DES2041-5248_g_des-pizza-slices-y6-test_meds-pizza-slices-range0000-0200.fits.fz \
+    DES2041-5248_r_des-pizza-slices-y6-test_meds-pizza-slices-range0000-0200.fits.fz \
+    DES2041-5248_i_des-pizza-slices-y6-test_meds-pizza-slices-range0000-0200.fits.fz
     # --absscale 0.015 \
     # --scales 1.0,1.0,1.3
