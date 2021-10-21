434575bf5855b0989c52178a9514368b9bb067f3
Matthew R. Becker <becker.mr@gmail.com>
2021-10-21 12:52:49 -0500

    more b ugz

--- 2021_10_21_test_newest_code/run_coadding.sh
+++ 2021_10_21_test_newest_code/run_coadding.sh
@@ -4,7 +4,7 @@ for band in g r i z; do
   mkdir -p /data/beckermr/tmp/${band}
   des-pizza-cutter \
     --config des-pizza-slices-y6-test.yaml \
-    --info=${MEDS_DIR}/des-pizza-slices-y6-v9/pizza_cutter_info/DES2041-5248_${band}_pizza_cutter_info.yaml \
+    --info=${MEDS_DIR}/des-pizza-slices-y6-test/pizza_cutter_info/DES2041-5248_${band}_pizza_cutter_info.yaml \
     --output-path=`pwd` \
     --seed=3232 \
     --use-tmpdir \
--- 2021_10_21_test_newest_code/run_img.sh
+++ 2021_10_21_test_newest_code/run_img.sh
@@ -2,8 +2,8 @@
 
 des-make-image-fromfiles \
     coadd-riz.jpg \
-    DES2041-5248_g_des-pizza-slices-y6-v9_meds-pizza-slices-coadd-img.fits.fz \
-    DES2041-5248_r_des-pizza-slices-y6-v9_meds-pizza-slices-coadd-img.fits.fz \
-    DES2041-5248_i_des-pizza-slices-y6-v9_meds-pizza-slices-coadd-img.fits.fz
+    DES2041-5248_g_des-pizza-slices-y6-test_meds-pizza-slices-coadd-img.fits.fz \
+    DES2041-5248_r_des-pizza-slices-y6-test_meds-pizza-slices-coadd-img.fits.fz \
+    DES2041-5248_i_des-pizza-slices-y6-test_meds-pizza-slices-coadd-img.fits.fz
     # --absscale 0.015 \
     # --scales 1.0,1.0,1.3
