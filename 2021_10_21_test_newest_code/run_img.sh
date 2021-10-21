a72a275a84cffec31b1d68c8e50a467fd91de107
Matthew R. Becker <becker.mr@gmail.com>
2021-10-21 14:40:04 -0500

    bugz

--- 2021_10_21_test_newest_code/run_img.sh
+++ 2021_10_21_test_newest_code/run_img.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-des-make-image-fromfiles \
-    coadd-riz.jpg \
-    DES2041-5248_g_des-pizza-slices-y6-test_meds-pizza-slices-range0000-0200.fits.fz \
-    DES2041-5248_r_des-pizza-slices-y6-test_meds-pizza-slices-range0000-0200.fits.fz \
-    DES2041-5248_i_des-pizza-slices-y6-test_meds-pizza-slices-range0000-0200.fits.fz
-    # --absscale 0.015 \
-    # --scales 1.0,1.0,1.3
+for band in g r i z; do
+  make-coadd-image-from-slices \
+    DES2041-5248_${band}_des-pizza-slices-y6-test_meds-pizza-slices-range0000-0200.fits.fz
+done
+
+# des-make-image-fromfiles \
+#     coadd-riz.jpg \
+#     DES2041-5248_g_des-pizza-slices-y6-test_meds-pizza-slices-range0000-0200.fits.fz \
+#     DES2041-5248_r_des-pizza-slices-y6-test_meds-pizza-slices-range0000-0200.fits.fz \
+#     DES2041-5248_i_des-pizza-slices-y6-test_meds-pizza-slices-range0000-0200.fits.fz
+#     # --absscale 0.015 \
+#     # --scales 1.0,1.0,1.3
