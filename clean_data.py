94bafbd98bedbfd2b3c15399a89490d8dc4660e5
Matthew R. Becker <becker.mr@gmail.com>
2021-11-17 16:12:54 -0600

    clear the notebooks

--- clean_data.py
+++ clean_data.py
@@ -4,5 +4,9 @@ import tqdm
 
 fnames = glob.glob("**/*.fit*", recursive=True)
 
-for fname in tqdm.tqdm(fnames):
+for fname in tqdm.tqdm(fnames, desc="removing fits files"):
     os.system("rm -f " + fname)
+
+fnames = glob.glob("**/*.ipynb", recursive=True)
+for fname in tqdm.tqdm(fnames, desc="clearing notebooks"):
+    os.system("jupyter nbconvert --clear-output --inplace " + fname)
