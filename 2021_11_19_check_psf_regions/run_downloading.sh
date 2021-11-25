9721e887d1b7234e75963b5d5b77366ec05c95f5
Matthew R. Becker <becker.mr@gmail.com>
2021-11-25 13:03:19 -0500

    ENH do the thing

---
+++ 2021_11_19_check_psf_regions/run_downloading.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# for band in g r i z; do
+#   des-pizza-cutter-prep-tile \
+#     --config des-pizza-slices-y6-test.yaml \
+#     --tilename DES0137-3749 \
+#     --band ${band}
+# done
+
+for tname in DES0131-3206 DES0137-3749 DES0221-0750 DES0229-0416; do
+  for band in g r i z; do
+    des-pizza-cutter-prep-tile \
+      --config des-pizza-slices-y6-test.yaml \
+      --tilename ${tname} \
+      --band ${band}
+  done
+done
