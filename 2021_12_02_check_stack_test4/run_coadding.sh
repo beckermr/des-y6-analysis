2d6ea23680fca82b1f0fdf4e50f4074c222a5371
Matthew R. Becker <becker.mr@gmail.com>
2021-12-02 14:50:52 -0600

    do the thing

--- 2021_12_02_check_stack_test4/run_coadding.sh
+++ 2021_12_02_check_stack_test4/run_coadding.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
-for band in r; do
+for band in g r i z; do
   des-pizza-cutter \
     --config des-pizza-slices-y6-test.yaml \
     --info=${MEDS_DIR}/des-pizza-slices-y6-test/pizza_cutter_info/DES0221-0750_${band}_pizza_cutter_info.yaml \
     --output-path=`pwd` \
     --seed=3232 \
-    --log-level=DEBUG \
-    --range="9005:9010"
-    # --n-jobs=6 \
-    # --n-chunks=12
+    --n-jobs=6 \
+    --n-chunks=12
   # --range="706:710" \
 done
