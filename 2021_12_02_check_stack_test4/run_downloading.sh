f2508babe7899b98e1b0791127ad3f38b468e35e
Matthew R. Becker <becker.mr@gmail.com>
2021-12-02 13:08:26 -0600

    do it all

--- 2021_12_02_check_stack_test4/run_downloading.sh
+++ 2021_12_02_check_stack_test4/run_downloading.sh
@@ -1,17 +1,10 @@
 #!/usr/bin/env bash
 
-for band in r; do
-  des-pizza-cutter-prep-tile \
-    --config des-pizza-slices-y6-test.yaml \
-    --tilename DES0221-0750 \
-    --band ${band}
+for tname in DES0131-3206 DES0137-3749 DES0221-0750 DES0229-0416; do
+  for band in g r i z; do
+    des-pizza-cutter-prep-tile \
+      --config des-pizza-slices-y6-test.yaml \
+      --tilename ${tname} \
+      --band ${band}
+  done
 done
-
-# for tname in DES0131-3206 DES0137-3749 DES0221-0750 DES0229-0416; do
-#   for band in g r i z; do
-#     des-pizza-cutter-prep-tile \
-#       --config des-pizza-slices-y6-test.yaml \
-#       --tilename ${tname} \
-#       --band ${band}
-#   done
-# done
