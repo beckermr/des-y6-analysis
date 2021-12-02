df3ca2795fdd75e15a5e987ce0789f14bd69b56a
Matthew R. Becker <becker.mr@gmail.com>
2021-12-02 14:54:01 -0600

    try again

--- 2021_12_02_check_stack_test4/run_coadding.sh
+++ 2021_12_02_check_stack_test4/run_coadding.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 for band in g r i z; do
+  mkdir -p /data/beckermr/tmp/${band}
   des-pizza-cutter \
     --config des-pizza-slices-y6-test.yaml \
     --info=${MEDS_DIR}/des-pizza-slices-y6-test/pizza_cutter_info/DES0221-0750_${band}_pizza_cutter_info.yaml \
