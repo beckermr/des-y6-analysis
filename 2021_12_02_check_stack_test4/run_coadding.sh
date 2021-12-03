55295f6d9f82b05f9aa5530d3122646490712539
Matthew R. Becker <becker.mr@gmail.com>
2021-12-03 09:01:29 -0600

    try this

--- 2021_12_02_check_stack_test4/run_coadding.sh
+++ 2021_12_02_check_stack_test4/run_coadding.sh
@@ -7,7 +7,8 @@ for band in g r i z; do
     --info=${MEDS_DIR}/des-pizza-slices-y6-test/pizza_cutter_info/DES0221-0750_${band}_pizza_cutter_info.yaml \
     --output-path=`pwd` \
     --seed=3232 \
-    --n-jobs=6 \
-    --n-chunks=12 \
-    --tmpdir=/data/beckermr/tmp/${band}
+    --n-jobs=1 \
+    --n-chunks=1 \
+    --tmpdir=/data/beckermr/tmp/${band} \
+    --range=10:200
 done
