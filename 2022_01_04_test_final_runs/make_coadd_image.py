011b86afd5e2cf34f7791cd85f8f37f491c84437
Matthew R. Becker <becker.mr@gmail.com>
2022-01-04 17:01:14 -0600

    make all the images

--- 2022_01_04_test_final_runs/make_coadd_image.py
+++ 2022_01_04_test_final_runs/make_coadd_image.py
@@ -2,59 +2,67 @@ import sys
 import subprocess
 import glob
 import concurrent.futures
+import os
+import tqdm
 
 tname = sys.argv[1]
 
 mfiles = glob.glob(
     "data/"
-    "OPS/multiepoch/Y6A2_PIZZACUTTER/*/%s/*/pizza-cutter/"
-    "%s_r*_*_pizza-cutter-slices.fits.fz" % (
-        tname, tname,
-    )
+    "OPS/multiepoch/Y6A2_PIZZACUTTER/*/*/*/pizza-cutter/"
+    "*_pizza-cutter-slices.fits.fz"
 )
-mfiles = sorted(mfiles)
-mfiles = [
-    mfiles[0],
-    mfiles[2],
-    mfiles[1],
-    mfiles[3]
-]
+tnames = [os.path.basename(m).split("_")[0] for m in mfiles]
 
-subprocess.run("mkdir -p images", check=True, shell=True)
+for tname in tqdm.tqdm(tnames):
 
+    mfiles = glob.glob(
+        "data/"
+        "OPS/multiepoch/Y6A2_PIZZACUTTER/*/%s/*/pizza-cutter/"
+        "%s_r*_*_pizza-cutter-slices.fits.fz" % (
+            tname, tname,
+        )
+    )
+    mfiles = sorted(mfiles)
+    mfiles = [
+        mfiles[0],
+        mfiles[2],
+        mfiles[1],
+        mfiles[3]
+    ]
 
-def _render(fname, tname, band):
+    subprocess.run("mkdir -p images", check=True, shell=True)
+
+    def _render(fname, tname, band):
+        subprocess.run(
+            "make-coadd-image-from-slices %s --output-path=images/%s-%s.fits.fz" % (
+                fname, tname, band,
+            ),
+            check=True,
+            shell=True,
+        )
+        return band
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+        futs = []
+        for band, fname in zip(["g", "r", "i", "z"], mfiles):
+            assert fname.endswith("%s_pizza-cutter-slices.fits.fz" % band)
+            futs.append(executor.submit(_render, fname, tname, band))
+
+        for fut in concurrent.futures.as_completed(futs):
+            print("done %s" % fut.result())
+
     subprocess.run(
-        "make-coadd-image-from-slices %s --output-path=images/%s-%s.fits.fz" % (
-            fname, tname, band,
-        ),
+        """\
+    des-make-image-fromfiles \
+        images/%s-coadd-gri.jpg \
+        images/%s-g.fits.fz \
+        images/%s-r.fits.fz \
+        images/%s-i.fits.fz \
+    """ % (tname, tname, tname, tname),
         check=True,
         shell=True,
     )
-    return band
-
-
-with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
-    futs = []
-    for band, fname in zip(["g", "r", "i", "z"], mfiles):
-        assert fname.endswith("%s_pizza-cutter-slices.fits.fz" % band)
-        futs.append(executor.submit(_render, fname, tname, band))
-
-    for fut in concurrent.futures.as_completed(futs):
-        print("done %s" % fut.result())
-
-
-subprocess.run(
-    """\
-des-make-image-fromfiles \
-    images/%s-coadd-gri.jpg \
-    images/%s-g.fits.fz \
-    images/%s-r.fits.fz \
-    images/%s-i.fits.fz \
-""" % (tname, tname, tname, tname),
-    check=True,
-    shell=True,
-)
 
 #     # --absscale 0.015 \
 #     # --scales 1.0,1.0,1.3
