ccaff54d03ed6c16b632c9cffecc66348f4ea547
Matthew R. Becker <becker.mr@gmail.com>
2022-01-04 13:19:40 -0600

    start images

---
+++ 2022_01_04_test_final_runs/make_coadd_image.py
@@ -0,0 +1,30 @@
+import sys
+import subprocess
+import glob
+import concurrent.futures
+
+tname = sys.argv[1]
+
+mfiles = glob.glob(
+    "data/"
+    "OPS/multiepoch/Y6A2_PIZZACUTTER/*/%s/*/pizza-cutter/"
+    "%s_r*_*_pizza-cutter-slices.fits.fz" % (
+        tname, tname,
+    )
+)
+mfiles = sorted(mfiles)
+
+print(mfiles)
+
+# for band in g r i z; do
+#   make-coadd-image-from-slices \
+#     DES2041-5248_${band}_des-pizza-slices-y6-test_meds-pizza-slices-range0000-0200.fits.fz
+# done
+#
+# des-make-image-fromfiles \
+#     coadd-riz.jpg \
+#     DES2041-5248_g_des-pizza-slices-y6-test_meds-pizza-slices-range0000-0200-coadd-img.fits.fz \
+#     DES2041-5248_r_des-pizza-slices-y6-test_meds-pizza-slices-range0000-0200-coadd-img.fits.fz \
+#     DES2041-5248_i_des-pizza-slices-y6-test_meds-pizza-slices-range0000-0200-coadd-img.fits.fz
+#     # --absscale 0.015 \
+#     # --scales 1.0,1.0,1.3
