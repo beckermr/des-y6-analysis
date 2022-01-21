28cefeb49eccf684a762f7f51ddcaa869bcfe8dc
Matthew R. Becker <becker.mr@gmail.com>
2022-01-21 07:03:00 -0600

    try it

---
+++ 2022_01_20_download_cats/cut_and_blind_cats.py
@@ -0,0 +1,43 @@
+import os
+import glob
+import fitsio
+import io
+import contextlib
+from ngmix.shape import (
+    e1e2_to_g1g2, g1g2_to_e1e2, g1g2_to_eta1eta2, eta1eta2_to_g1g2
+)
+
+from des_y6utils.shear_masking import generate_shear_masking_factor
+
+os.makedirs("data_final", exist_ok=True)
+
+with open(os.path.expanduser("~/.test_des_blinding_v1"), "r") as fp:
+    passphrase = fp.read().strip()
+
+fac = generate_shear_masking_factor(passphrase)
+
+fnames = glob.glob("data/**/*.fit*", recursive=True)
+
+for fname in fnames:
+    buff = io.StringIO()
+    with contextlib.redirect_stdout(buff):
+        with contextlib.redirect_stderr(buff):
+            try:
+                d = fitsio.read(fname)
+                msk = d["flags"] == 0
+                d = d[msk]
+
+                msk = d["shear_step"] == "no_shear"
+                e1, e2 = d["mdet_g_1"][msk], d["mdet_g_2"][msk]
+                g1, g2 = e1e2_to_g1g2(e1, e2)
+                eta1, eta2 = g1g2_to_eta1eta2(g1, g2)
+                eta1 *= fac
+                eta2 *= fac
+                g1, g2 = eta1eta2_to_g1g2(eta1, eta2)
+                e1, e2 = g1g2_to_e1e2(g1, g2)
+                d["mdet_g_1"][msk] = e1
+                d["mdet_g_2"][msk] = e2
+                out = os.path.join("data_final", os.path.basename(fname))
+                fitsio.write(out, d, clobber=True)
+            except Exception:
+                raise RuntimeError("failed!")
