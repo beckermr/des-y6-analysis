ff1ee700be47895203ac82c463969f51218f2b02
Matthew R. Becker <becker.mr@gmail.com>
2022-01-22 12:55:41 -0600

    try this

---
+++ 2022_01_22_run_mdet_final_v1/run_tile.py
@@ -0,0 +1,12 @@
+import fitsio
+import subprocess
+from concurrent.futures import as_completed
+from esutil.pbar import PBar
+from condor_exec import CondorExecutor
+
+
+def _run_tile(tilename):
+    pass
+
+
+conda_env = "des-y6-final-v1"
