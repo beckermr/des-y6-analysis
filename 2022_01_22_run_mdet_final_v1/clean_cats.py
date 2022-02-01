8ebfc9c16f82afa12b42c507b76637df611590f4
Matthew R. Becker <becker.mr@gmail.com>
2022-02-01 17:54:44 -0600

    script

--- 2022_01_22_run_mdet_final_v1/clean_cats.py
+++ 2022_01_22_run_mdet_final_v1/clean_cats.py
@@ -11,7 +11,6 @@ def _is_ok(fname):
             fitsio.read(fname)
             return True
         except Exception:
-            print("\n\n\n" + fname + "\n\n\n", flush=True)
             return False
     else:
         return False
@@ -21,12 +20,12 @@ if __name__ == "__main__":
     fnames = sorted(glob.glob("mdet_data/*.fit*", recursive=True))
     print("found %d tiles to process" % len(fnames), flush=True)
     with ProcessPoolExecutor(max_workers=8) as exec:
-        futs = [
-            exec.submit(_is_ok, fname)
+        futs = {
+            exec.submit(_is_ok, fname): fname
             for fname in tqdm.tqdm(fnames, desc="making jobs")
-        ]
+        }
         for fut in tqdm.tqdm(as_completed(futs), total=len(futs)):
             try:
                 fut.result()
             except Exception:
-                pass
+                print("\n\n\n" + futs[fut] + "\n\n\n", flush=True)
