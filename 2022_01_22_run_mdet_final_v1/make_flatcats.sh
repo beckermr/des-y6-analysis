d3f06f405a64dce17161649bcdc9b720bf9c5d46
Matthew R. Becker <becker.mr@gmail.com>
2022-02-01 16:12:44 -0600

    script

--- 2022_01_22_run_mdet_final_v1/make_flatcats.sh
+++ 2022_01_22_run_mdet_final_v1/make_flatcats.sh
@@ -4,4 +4,4 @@ des-y6-make-mdet-hdf5-flatcat \
   --input-glob="./mdet_data/*.fits.fz" \
   --output="${HOME}/desdata/des-sims/mdet_output_v1.h5" \
   --passphrase-file="${HOME}/.test_des_blinding_v1" \
-  --cols-per-io-pass=5
+  --cols-per-io-pass=1
