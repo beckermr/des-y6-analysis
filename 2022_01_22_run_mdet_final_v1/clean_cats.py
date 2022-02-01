97f354fd061b037a9a00474987e644a572bd913e
Matthew R. Becker <becker.mr@gmail.com>
2022-02-01 16:54:15 -0600

    script

---
+++ 2022_01_22_run_mdet_final_v1/clean_cats.py
@@ -0,0 +1,32 @@
+import os
+import glob
+import fitsio
+import tqdm
+from concurrent.futures import ProcessPoolExecutor, as_completed
+
+
+def _is_ok(fname):
+    if os.path.exists(fname):
+        try:
+            fitsio.read(fname)
+            return True
+        except Exception:
+            print(fname, flush=True)
+            return False
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    fnames = glob.glob("mdet_data/*.fit*", recursive=True)
+    print("found %d tiles to process" % len(fnames), flush=True)
+    with ProcessPoolExecutor(max_workers=8) as exec:
+        futs = [
+            exec.submit(_is_ok, fname)
+            for fname in tqdm.tqdm(fnames, desc="making jobs")
+        ]
+        for fut in tqdm.tqdm(as_completed(futs), total=len(futs)):
+            try:
+                fut.result()
+            except Exception:
+                pass
