c5d621d6424cf0a526a5dbb302472f3595170472
Matthew R. Becker <becker.mr@gmail.com>
2022-02-01 18:05:42 -0600

    script

--- 2022_01_22_run_mdet_final_v1/clean_cats.py
+++ 2022_01_22_run_mdet_final_v1/clean_cats.py
@@ -8,14 +8,14 @@ def _is_ok(fname):
     subprocess.run(
         "python -c 'import fitsio; fitsio.read(\"%s\")'" % fname,
         shell=True,
-        check=True
+        check=True,
     )
 
 
 if __name__ == "__main__":
     fnames = sorted(glob.glob("mdet_data/*.fit*", recursive=True))
     print("found %d tiles to process" % len(fnames), flush=True)
-    with ProcessPoolExecutor(max_workers=8) as exec:
+    with ProcessPoolExecutor(max_workers=4) as exec:
         futs = {
             exec.submit(_is_ok, fname): fname
             for fname in tqdm.tqdm(fnames, desc="making jobs")
