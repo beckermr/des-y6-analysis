5137e0e78c58ffac1ed9302b7aa7acd9e351c253
Matthew R. Becker <becker.mr@gmail.com>
2022-02-07 13:19:17 -0600

    download the rest

--- 2022_02_04_imsims_tests_desy6/run_downloading.sh
+++ 2022_02_04_imsims_tests_desy6/run_downloading.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for tname in DES2359-6539; do
-  for band in i; do
+  for band in r i z; do
     des-pizza-cutter-prep-tile \
       --config des-pizza-slices-y6-v12.yaml \
       --tilename ${tname} \
