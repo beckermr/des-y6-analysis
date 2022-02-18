7c2933c4123070dd742e716d675412c728b6d8a5
Matthew R. Becker <becker.mr@gmail.com>
2022-02-18 12:56:23 -0600

    ENH do the thing

--- 2022_02_17_se_pgauss_cte/run_tiles.py
+++ 2022_02_17_se_pgauss_cte/run_tiles.py
@@ -125,7 +125,7 @@ def _run_tile(tilename, band, seed, cwd):
                             data["row"][dind] += rr
                             data["n"][dind] += 1
 
-            if np.sum(data["n"]) > 1000:
+            if np.sum(data["n"]) > 10000:
                 break
 
     return data
