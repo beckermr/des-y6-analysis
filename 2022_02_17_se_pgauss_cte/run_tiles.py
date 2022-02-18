90030c1eccfb2656720dfc00f1290722581518a2
Matthew R. Becker <becker.mr@gmail.com>
2022-02-18 12:09:39 -0600

    ENH do the thing

--- 2022_02_17_se_pgauss_cte/run_tiles.py
+++ 2022_02_17_se_pgauss_cte/run_tiles.py
@@ -117,7 +117,7 @@ def _run_tile(tilename, band, seed, cwd):
 
                             bin = max(min(rr-1, 4095), 0) // 128
                             dind = (ccdnum-1)*32 + bin
-                            assert data["ccdnum"][dind] == "ccdnum"
+                            assert data["ccdnum"][dind] == ccdnum
                             assert data["bin"][dind] == bin
                             data["e1"][dind] += res["e"][0]
                             data["row"][dind] += rr
