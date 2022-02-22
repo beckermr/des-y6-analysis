93d3d7fee25ff5a1eca60f7f0841e5df7ceb5aa9
Matthew R. Becker <becker.mr@gmail.com>
2022-02-22 13:59:16 -0600

    try this

--- 2022_02_17_se_pgauss_cte/reduce_cte_data.py
+++ 2022_02_17_se_pgauss_cte/reduce_cte_data.py
@@ -160,6 +160,8 @@ def _reduce_rows_cols(fnames, shape, col, desc, loc_col, oname):
                 ns[b] = np.sum(msk)
 
     print(n)
+    print(e1, e2)
+    print(e1_err, e2_err)
 
     e1_err = np.sqrt(e1_err / (n - 1))
     e2_err = np.sqrt(e2_err / (n - 1))
