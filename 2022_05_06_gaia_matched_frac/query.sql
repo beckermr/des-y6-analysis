29de0cbd4b26d226742ecc5bd72c4da99c681d65
Matthew R. Becker <becker.mr@gmail.com>
2022-05-06 17:02:00 -0500

    unload

--- 2022_05_06_gaia_matched_frac/query.sql
+++ 2022_05_06_gaia_matched_frac/query.sql
@@ -1,13 +1,20 @@
 select
-  count(*)
+  d.ext_mash,
+  g.PHOT_G_MEAN_MAG,
+  d.MAG_AUTO_G,
+  d.MAG_AUTO_R,
+  d.MAG_AUTO_I,
+  d.MAG_AUTO_Z,
+  d.COADD_OBJECT_ID,
+  g.SOURCE_ID
 from
   GAIA_DR2_X_Y6_GOLD_2_0 m,
   GAIA_DR2 g,
   Y6_GOLD_2_0 d
 where
   m.COADD_OBJECT_ID = d.COADD_OBJECT_ID
+  and ROWNUM < 10
   and m.SOURCE_ID = g.SOURCE_ID
   and d.flags_footprint > 0
   and d.flags_gold = 0
-  and d.flags_foreground = 0
-  and d.ext_mash = 4;
+  and d.flags_foreground = 0; > gaia.fits
