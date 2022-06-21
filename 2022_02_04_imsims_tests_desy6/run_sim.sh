6b6f5bb98cf707be670c10e793013db88a867121
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2022-06-21 11:49:29 -0500

    wrong step

--- 2022_02_04_imsims_tests_desy6/run_sim.sh
+++ 2022_02_04_imsims_tests_desy6/run_sim.sh
@@ -4,8 +4,7 @@ export IMSIM_DATA=${MEDS_DIR}
 
 mkdir -p ./sim_outputs
 run-eastlake-sim \
-  -v 1 \
-  -v 1 \
+  -v 2 \
   --skip_completed_steps \
   --resume_from=./sim_outputs/job_record.pkl \
   config.yaml \
