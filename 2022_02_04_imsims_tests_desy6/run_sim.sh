2f06c2d030a59828af1e57649dc0099428f2d863
Matthew R Becker <becker.mr@gmail.com>
2022-06-21 12:41:29 -0400

    ENH latest changes

--- 2022_02_04_imsims_tests_desy6/run_sim.sh
+++ 2022_02_04_imsims_tests_desy6/run_sim.sh
@@ -5,7 +5,11 @@ export IMSIM_DATA=${MEDS_DIR}
 mkdir -p ./sim_outputs
 run-eastlake-sim \
   -v 1 \
-  --skip_completed_steps \
-  --resume_from=./sim_outputs/job_record.pkl \
   config.yaml \
   ./sim_outputs
+
+
+#  --step_names src_extractor \
+#  --resume_from=./sim_outputs/job_record.pkl \
+
+#  --skip_completed_steps \
