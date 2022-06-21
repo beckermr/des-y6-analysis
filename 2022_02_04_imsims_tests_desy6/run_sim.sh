10177d824dfa09ecb1e897ddfdffbbae5d84c070
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2022-06-21 11:44:20 -0500

    add configs

--- 2022_02_04_imsims_tests_desy6/run_sim.sh
+++ 2022_02_04_imsims_tests_desy6/run_sim.sh
@@ -5,6 +5,9 @@ export IMSIM_DATA=${MEDS_DIR}
 mkdir -p ./sim_outputs
 run-eastlake-sim \
   -v 1 \
+  -v 1 \
+  --skip_completed_steps \
+  --resume_from=./sim_outputs/job_record.pkl \
   config.yaml \
   ./sim_outputs
 
