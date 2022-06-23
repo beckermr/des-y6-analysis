f19f8c71c84beb31e9ea36b3db92a2cd555d7e2e
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2022-06-23 16:01:54 -0400

    restart

--- 2022_02_04_imsims_tests_desy6/run_sim.sh
+++ 2022_02_04_imsims_tests_desy6/run_sim.sh
@@ -5,13 +5,13 @@ export IMSIM_DATA=${MEDS_DIR}
 mkdir -p ./sim_outputs
 run-eastlake-sim \
   -v 1 \
+  --step_names galsim_montara pizza_cutter \
+  --resume_from=./sim_outputs/job_record.pkl \
   config.yaml \
   ./sim_outputs
 
 
 
-  # --step_names galsim_montara pizza_cutter \
-  # --resume_from=./sim_outputs/job_record.pkl \
 
 #  --step_names src_extractor \
 #  --resume_from=./sim_outputs/job_record.pkl \
