bc6fa3f21950dc527d33a7c5d4f3ed62b70436e5
beckermr <becker.mr@gmail.com>
2022-07-11 15:44:51 -0500

    new version

--- 2022_07_11_imsims_tests_desy6/run_downloading.sh
+++ 2022_07_11_imsims_tests_desy6/run_downloading.sh
@@ -3,7 +3,7 @@
 for tname in DES2359-6539; do
   for band in r i z; do
     des-pizza-cutter-prep-tile \
-      --config des-pizza-slices-y6-v12.yaml \
+      --config des-pizza-slices-y6-v14.yaml \
       --tilename ${tname} \
       --band ${band}
   done
