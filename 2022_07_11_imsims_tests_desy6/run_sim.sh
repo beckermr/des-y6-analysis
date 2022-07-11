d444bce258b55f87f8a2a776f79dbcfe27f6ae7e
beckermr <becker.mr@gmail.com>
2022-07-11 16:28:36 -0500

    try again

--- 2022_07_11_imsims_tests_desy6/run_sim.sh
+++ 2022_07_11_imsims_tests_desy6/run_sim.sh
@@ -6,16 +6,14 @@ mkdir -p ./sim_outputs
 run-eastlake-sim \
   -v 1 \
   --seed 234324 \
-  --skip_completed_steps \
-  --resume_from=./sim_outputs/job_record.pkl \
   config.yaml \
   ./sim_outputs
 
   # --step_names pizza_cutter metadetect \
   # --resume_from=./sim_outputs/job_record.pkl \
 
-
-
+  # --skip_completed_steps \
+  # --resume_from=./sim_outputs/job_record.pkl \
 
 #  --step_names src_extractor \
 #  --resume_from=./sim_outputs/job_record.pkl \
