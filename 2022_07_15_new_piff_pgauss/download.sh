3a52eeb8be74f62810c014b66d34398d664c1972
beckermr <becker.mr@gmail.com>
2022-07-15 13:25:07 -0500

    download

--- 2022_07_15_new_piff_pgauss/download.sh
+++ 2022_07_15_new_piff_pgauss/download.sh
@@ -6,6 +6,19 @@ for fname in \
   OPS/multiepoch/Y6A2_PIZZACUTTER/r5922/DES0322-2249/p04/pizza-cutter/DES0322-2249_r5922p04_metadetect.fits.fz \
   OPS/multiepoch/Y6A2_PIZZACUTTER/r5922/DES0322-2249/p04/pizza-cutter/DES0322-2249_r5922p04_r_pizza-cutter-slices.fits.fz \
   OPS/multiepoch/Y6A2_PIZZACUTTER/r5922/DES0322-2249/p04/pizza-cutter/DES0322-2249_r5922p04_z_pizza-cutter-slices.fits.fz \
+\
+  OPS/multiepoch/Y6A2_PIZZACUTTER/r5922/DES0325-2249/p02/pizza-cutter/DES0325-2249_r5922p02_g_pizza-cutter-slices.fits.fz \
+  OPS/multiepoch/Y6A2_PIZZACUTTER/r5922/DES0325-2249/p02/pizza-cutter/DES0325-2249_r5922p02_i_pizza-cutter-slices.fits.fz \
+  OPS/multiepoch/Y6A2_PIZZACUTTER/r5922/DES0325-2249/p02/pizza-cutter/DES0325-2249_r5922p02_metadetect.fits.fz \
+  OPS/multiepoch/Y6A2_PIZZACUTTER/r5922/DES0325-2249/p02/pizza-cutter/DES0325-2249_r5922p02_r_pizza-cutter-slices.fits.fz \
+  OPS/multiepoch/Y6A2_PIZZACUTTER/r5922/DES0325-2249/p02/pizza-cutter/DES0325-2249_r5922p02_z_pizza-cutter-slices.fits.fz \
+\
+  OPS/multiepoch/Y6A2_PIZZACUTTER/r5922/DES0328-2249/p02/pizza-cutter/DES0328-2249_r5922p02_g_pizza-cutter-slices.fits.fz \
+  OPS/multiepoch/Y6A2_PIZZACUTTER/r5922/DES0328-2249/p02/pizza-cutter/DES0328-2249_r5922p02_i_pizza-cutter-slices.fits.fz \
+  OPS/multiepoch/Y6A2_PIZZACUTTER/r5922/DES0328-2249/p02/pizza-cutter/DES0328-2249_r5922p02_metadetect.fits.fz \
+  OPS/multiepoch/Y6A2_PIZZACUTTER/r5922/DES0328-2249/p02/pizza-cutter/DES0328-2249_r5922p02_r_pizza-cutter-slices.fits.fz \
+  OPS/multiepoch/Y6A2_PIZZACUTTER/r5922/DES0328-2249/p02/pizza-cutter/DES0328-2249_r5922p02_z_pizza-cutter-slices.fits.fz \
+\
 ; do
 
   rsync \
