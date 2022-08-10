94f0924c868fb8038c84f98696fdbb9ca51542e8
beckermr <becker.mr@gmail.com>
2022-08-10 14:55:00 -0500

    try more

--- 2022_08_10_test_new_mdet_stack/run_tile.py
+++ 2022_08_10_test_new_mdet_stack/run_tile.py
@@ -76,7 +76,6 @@ run-metadetect-on-slices \
   --tmpdir=%s \
   --log-level=INFO \
   --n-jobs=12 \
-  --range=0:10 \
   --band-names=griz %s %s %s %s""" % (
         cwd, seed, tmpdir, mfiles[0], mfiles[1], mfiles[2], mfiles[3]
     )
