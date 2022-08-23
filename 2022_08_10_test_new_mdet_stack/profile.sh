4b2d96d5ef5bbb42e88e745ad485d4c6691a80f8
beckermr <becker.mr@gmail.com>
2022-08-23 10:46:53 -0500

    try this

---
+++ 2022_08_10_test_new_mdet_stack/profile.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+python -m cProfile -o profile.dat `which run-metadetect-on-slices` \
+  --config=metadetect-v6-all-meas.yaml \
+  --output-path=./mdet_data \
+  --seed=1342 \
+  --use-tmpdir \
+  --tmpdir=${TMPDIR} \
+  --log-level=INFO \
+  --n-jobs=1 \
+  --band-names=griz \
+  --range="0:20" \
+  DES0328-2249_r5922p01_g_pizza-cutter-slices.fits.fz \
+  DES0328-2249_r5922p01_r_pizza-cutter-slices.fits.fz \
+  DES0328-2249_r5922p01_i_pizza-cutter-slices.fits.fz \
+  DES0328-2249_r5922p01_z_pizza-cutter-slices.fits.fz
