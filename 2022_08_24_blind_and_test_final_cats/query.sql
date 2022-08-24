7daff80adf9447502a8bb9139307ac92691e63b1
beckermr <becker.mr@gmail.com>
2022-08-24 08:58:47 -0500

    ENH try again

---
+++ 2022_08_24_blind_and_test_final_cats/query.sql
@@ -0,0 +1,16 @@
+select
+    concat(fai.filename, fai.compression) as filename,
+    fai.path as path,
+    m.band as band
+from
+    desfile d1,
+    proctag t,
+    miscfile m,
+    file_archive_info fai
+where
+    d1.pfw_attempt_id = t.pfw_attempt_id
+    and t.tag = 'Y6A2_METADETECT_TEST_V3_3'
+    and d1.filename = m.filename
+    and d1.id = fai.desfile_id
+    and fai.archive_name = 'desar2home'
+    and d1.filetype = 'coadd_metadetect'; > fnames.fits
