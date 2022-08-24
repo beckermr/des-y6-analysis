ce69e38122ebddf206e3b4688b49b542d710f3ce
beckermr <becker.mr@gmail.com>
2022-08-24 14:52:30 -0500

    try latest

--- 2022_08_10_test_new_mdet_stack/profile.sh
+++ 2022_08_10_test_new_mdet_stack/profile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python -m cProfile -o profile_ppsfperfv1.dat `which run-metadetect-on-slices` \
+python -m cProfile -o prof_ppsfperfv2.dat `which run-metadetect-on-slices` \
   --config=metadetect-v6-all-meas.yaml \
   --output-path=./mdet_data \
   --seed=1342 \
