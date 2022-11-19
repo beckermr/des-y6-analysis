c8cd17601db3a344b04ddcdd084c2e9650ba5a36
beckermr <becker.mr@gmail.com>
2022-11-19 10:54:08 -0600

    tests for sep and weight opt

---
+++ 2022_22_11_18_sep_deblend_cont/query.sql
@@ -0,0 +1,17 @@
+select
+    concat(fai.filename, fai.compression) as filename,
+    fai.path as path,
+    m.band as band
+from
+    desfile d1,
+    proctag t,
+    miscfile m,
+    file_archive_info fai
+where
+    d1.pfw_attempt_id = t.pfw_attempt_id
+    and t.tag = 'Y6A2_PIZZACUTTER_V3'
+    and d1.filename = m.filename
+    and d1.id = fai.desfile_id
+    and fai.archive_name = 'desar2home'
+    and m.band = 'r'
+    and d1.filetype = 'coadd_pizza_cutter'; > fnames.fits
