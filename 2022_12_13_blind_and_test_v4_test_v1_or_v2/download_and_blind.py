b462ba438077e62e7783831a3c72e7b4b4c3f5fd
beckermr <becker.mr@gmail.com>
2022-12-13 14:20:25 -0600

    ENH code to download and blind mdet cats

---
+++ 2022_12_13_blind_and_test_v4_test_v1_or_v2/download_and_blind.py
@@ -0,0 +1,179 @@
+from wurlitzer import sys_pipes
+import os
+import fitsio
+import subprocess
+import tqdm
+import io
+import sys
+import contextlib
+import numpy as np
+from ngmix.shape import g1g2_to_eta1eta2, eta1eta2_to_g1g2
+from concurrent.futures import ProcessPoolExecutor, as_completed
+
+from des_y6utils.shear_masking import generate_shear_masking_factor
+
+OUTDIR = "blinded_data_test_v1"
+COLS_TO_KEEP = ["pgauss", "gauss"]
+
+
+def _download(fname):
+    cmd = """\
+    rsync \
+            -av \
+            --password-file $DES_RSYNC_PASSFILE \
+            ${DESREMOTE_RSYNC_USER}@${DESREMOTE_RSYNC}/%s \
+            ./mdet_data/%s
+    """ % (fname, os.path.basename(fname))
+    subprocess.run(cmd, shell=True, check=True)
+
+
+def _is_ok(fname):
+    if os.path.exists(fname):
+        try:
+            fitsio.read_header(fname)
+            return True
+        except Exception:
+            return False
+    else:
+        return False
+
+
+def _msk_shear(fname, passphrase):
+    indata = os.path.join(".", "mdet_data", os.path.basename(fname))
+    outdata = os.path.join(".", OUTDIR, os.path.basename(fname))
+    if _is_ok(outdata):
+        return None
+
+    fac = generate_shear_masking_factor(passphrase)
+    failed = False
+    err = ""
+
+    try:
+        buff = io.StringIO()
+        with contextlib.redirect_stderr(sys.stdout):
+            with contextlib.redirect_stdout(buff):
+                with sys_pipes():
+                    _download(fname)
+    except Exception:
+        err = "download error"
+        failed = True
+
+    if not failed:
+        buff = io.StringIO()
+        with contextlib.redirect_stderr(sys.stdout):
+            with contextlib.redirect_stdout(buff):
+                try:
+                    d = fitsio.read(indata)
+
+                    # make all non-zero flags nan in shear
+                    for pre in COLS_TO_KEEP:
+                        msk = (d[pre + "_flags"] != 0)
+                        for col in [
+                            '_g_1',
+                            '_g_2',
+                            '_g_cov_1_1',
+                            '_g_cov_1_2',
+                            '_g_cov_2_2',
+                        ]:
+                            d[pre + col][msk] = np.nan
+
+                        # apply blinding to the good ones
+                        msk = (
+                            (d["mdet_step"] == "noshear")
+                            & (d[pre + "_flags"] == 0)
+                        )
+                        e1o, e2o = (
+                            d[pre + "_g_1"][msk].copy(),
+                            d[pre + "_g_2"][msk].copy(),
+                        )
+                        if pre not in ["gauss"]:
+                            e1 = e1o * fac
+                            e2 = e2o * fac
+                        else:
+                            # use eta due to bounds
+                            eta1o, eta2o = g1g2_to_eta1eta2(e1o, e2o)
+                            eta1 = eta1o * fac
+                            eta2 = eta2o * fac
+                            e1, e2 = eta1eta2_to_g1g2(eta1, eta2)
+
+                        d[pre + "_g_1"][msk] = e1
+                        d[pre + "_g_2"][msk] = e2
+
+                        assert not np.array_equal(d[pre + "_g_1"][msk], e1o)
+                        assert not np.array_equal(d[pre + "_g_2"][msk], e2o)
+
+                    msk = None
+                    for pre in COLS_TO_KEEP:
+                        _msk = (
+                            (d[pre + "_flags"] == 0)
+                            & (d[pre + "_s2n"] >= 5.0)
+                        )
+                        if msk is None:
+                            msk = _msk
+                        else:
+                            msk |= _msk
+
+                    msk &= (d["mask_flags"] == 0)
+                    fitsio.write(outdata, d[msk], clobber=True)
+
+                except Exception:
+                    failed = True
+                    err = "blinding error"
+
+    if failed:
+        print("\ntile %s failed! - %s" % (fname, err), flush=True)
+    else:
+        pass
+        # print("copied tile %s!" % fname, flush=True)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        num = int(sys.argv[1])
+        tot = int(sys.argv[2])
+    else:
+        num = 0
+        tot = 1
+
+    if not os.path.exists(OUTDIR):
+        os.makedirs(OUTDIR, exist_ok=True)
+    if not os.path.exists("./mdet_data"):
+        os.makedirs("./mdet_data", exist_ok=True)
+        os.system("chmod go-r ./mdet_data")
+
+    with open(os.path.expanduser("~/.test_des_blinding_v7"), "r") as fp:
+        passphrase = fp.read().strip()
+
+    d = fitsio.read("fnames.fits", lower=True)
+
+    _fnames = sorted([
+        os.path.join(d["path"][i], d["filename"][i])
+        for i in range(len(d))
+    ])
+    fnames = [
+        fname
+        for i, fname in enumerate(_fnames)
+        if i % tot == num
+    ]
+
+    print(
+        "found %d tiles to process for task %d" % (
+            len(fnames), num
+        ),
+        flush=True,
+    )
+    with ProcessPoolExecutor(max_workers=5) as exec:
+        futs = [
+            exec.submit(_msk_shear, fname, passphrase)
+            for fname in tqdm.tqdm(fnames, desc="making jobs")
+        ]
+        for fut in tqdm.tqdm(
+            as_completed(futs), total=len(futs), desc="making catalogs"
+        ):
+            try:
+                fut.result()
+            except Exception as e:
+                print(e)
+
+            if num == 0:
+                os.system(f"cd {OUTDIR} && ls -1 *.fits > mdet_files.txt")
---
+++ 2022_12_13_blind_and_test_v4_test_v1_or_v2/sub_jobs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+tot=4
+
+for i in `seq 1 ${tot}`; do
+  num=$((i-1))
+  echo "\
+N: 1
+mode: by_node
+command: |
+  source ~/.bashrc
+  conda activate desy6
+  echo \`which python\`
+  python download_and_blind.py ${num} ${tot} &> log${num}.oe
+" > wq_sub${num}.yaml
+  wq sub -b wq_sub${num}.yaml
+done
