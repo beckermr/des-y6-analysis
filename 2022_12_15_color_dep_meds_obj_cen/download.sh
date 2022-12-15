8a5738f789204a50e94d5cf2ea4c65b0fe08fd83
beckermr <becker.mr@gmail.com>
2022-12-15 05:58:20 -0600

    ENH new code for testing

---
+++ 2022_12_15_color_dep_meds_obj_cen/download.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir -p meds_dir
+export MEDS_DIR=./meds_dir
+
+for band in g r i z; do
+  desmeds-prep-tile \
+    --no-temp \
+    --medsconf=Y6A1_test_piff.yaml \
+    --tilename=DES0137-3749 \
+    --band=${band}
+done
---
+++ 2022_12_15_color_dep_meds_obj_cen/make.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir -p meds_dir
+export MEDS_DIR=./meds_dir
+
+tmpmeds=/data/beckermr/2022_09_20_color_dep_meds/DES0137-3749
+mkdir -p ${tmpmeds}
+rm -rf ${tmpmeds}/*
+
+for band in g r i z; do
+  desmeds-make-meds-desdm \
+    Y6A1_test_piff.yaml \
+    ${MEDS_DIR}/Y6A1_test_piff/DES0137-3749/lists-${band}/DES0137-3749_${band}_fileconf-Y6A1_test_piff.yaml \
+    --tmpdir=${tmpmeds} &> log.oe
+done
+
+rm -rf ${tmpmeds}
