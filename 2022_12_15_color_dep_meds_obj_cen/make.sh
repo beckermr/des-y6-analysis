5656cb3fb586f920706aa98a77c22f2fce45327a
beckermr <becker.mr@gmail.com>
2022-12-15 06:11:36 -0600

    logs

--- 2022_12_15_color_dep_meds_obj_cen/make.sh
+++ 2022_12_15_color_dep_meds_obj_cen/make.sh
@@ -3,7 +3,7 @@
 mkdir -p meds_dir
 export MEDS_DIR=./meds_dir
 
-tmpmeds=/data/beckermr/2022_09_20_color_dep_meds/DES0137-3749
+tmpmeds=/data/beckermr/2022_12_15_color_dep_meds_obj_cen/DES0137-3749
 mkdir -p ${tmpmeds}
 rm -rf ${tmpmeds}/*
 
@@ -11,7 +11,7 @@ for band in g r i z; do
   desmeds-make-meds-desdm \
     Y6A1_test_piff.yaml \
     ${MEDS_DIR}/Y6A1_test_piff/DES0137-3749/lists-${band}/DES0137-3749_${band}_fileconf-Y6A1_test_piff.yaml \
-    --tmpdir=${tmpmeds} &> log.oe
+    --tmpdir=${tmpmeds} &> log${band}.oe
 done
 
 rm -rf ${tmpmeds}
