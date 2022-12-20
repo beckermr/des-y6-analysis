2a56930028249ca56cabfc27a9ed7db3abec62de
beckermr <becker.mr@gmail.com>
2022-12-20 10:22:35 -0600

    try another tile

--- 2022_12_15_color_dep_meds_obj_cen/download.sh
+++ 2022_12_15_color_dep_meds_obj_cen/download.sh
@@ -3,10 +3,12 @@
 mkdir -p meds_dir
 export MEDS_DIR=./meds_dir
 
+tname="DES0253+0500"
+
 for band in g r i z; do
   desmeds-prep-tile \
     --no-temp \
     --medsconf=Y6A1_test_piff.yaml \
-    --tilename=DES2214-5914 \
+    --tilename=${tname} \
     --band=${band}
 done
--- 2022_12_15_color_dep_meds_obj_cen/make.sh
+++ 2022_12_15_color_dep_meds_obj_cen/make.sh
@@ -3,14 +3,16 @@
 mkdir -p meds_dir
 export MEDS_DIR=./meds_dir
 
-tmpmeds=/data/beckermr/2022_12_15_color_dep_meds_obj_cen/DES2214-5914
+tname="DES0253+0500"
+
+tmpmeds=/data/beckermr/2022_12_15_color_dep_meds_obj_cen/${tname}
 mkdir -p ${tmpmeds}
 rm -rf ${tmpmeds}/*
 
 for band in g r i z; do
   desmeds-make-meds-desdm \
     Y6A1_test_piff.yaml \
-    ${MEDS_DIR}/Y6A1_test_piff/DES2214-5914/lists-${band}/DES2214-5914_${band}_fileconf-Y6A1_test_piff.yaml \
+    ${MEDS_DIR}/Y6A1_test_piff/${tname}/lists-${band}/${tname}_${band}_fileconf-Y6A1_test_piff.yaml \
     --tmpdir=${tmpmeds} &> log${band}.oe
 done
 
