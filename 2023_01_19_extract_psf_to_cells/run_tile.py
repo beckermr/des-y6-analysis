faff528acfbb76d719e6f7e157d88a9dd160cd02
beckermr <becker.mr@gmail.com>
2023-01-31 14:53:00 -0600

    run tiles

--- 2023_01_19_extract_psf_to_cells/run_tile.py
+++ 2023_01_19_extract_psf_to_cells/run_tile.py
@@ -3,17 +3,13 @@ import os
 import fitsio
 import subprocess
 import numpy as np
-import glob
-import time
-import random
-import joblib
 from esutil.pbar import PBar
 from mattspy import BNLCondorParallel
+import esutil
+import joblib
 
 
 def _download_tile(tilename, cwd):
-    os.system("mkdir -p data")
-
     d = fitsio.read(
         os.path.join(cwd, "fnames.fits"),
         lower=True,
@@ -48,117 +44,96 @@ ${DESREMOTE_RSYNC_USER}@${DESREMOTE_RSYNC}/%s \
     return mfiles
 
 
-def _run_tile(tilename, seed, opth, tmpdir, cwd, cpus):
-    os.system("mkdir -p ./data")
-    for _ in range(10):
-        try:
-            mfiles = _download_tile(tilename, cwd)
-            break
-        except Exception:
-            time.sleep(600 + random.randint(-100, 100))
-            mfiles = None
-            pass
+def _extract_expnum_ccdnum(ii):
+    expnums = np.zeros(len(ii), dtype=int) + -1
+    ccdnums = np.zeros(len(ii), dtype=int) + -1
+    for i in range(len(ii)):
+        ip = ii["image_path"][i]
+        if len(ip) > 0:
+            expnums[i] = int(os.path.basename(ip).split("_")[0][1:])
+            ccdnums[i] = int(os.path.basename(ip).split("_")[2][1:])
+    return expnums, ccdnums
 
-    if mfiles is None:
-        raise RuntimeError("Could not download files for tile %s" % tilename)
-    elif not isinstance(mfiles, list):
-        raise RuntimeError("Only found %d files for tile %s" % (
-            mfiles, tilename
-        ))
 
-    if tmpdir is None:
-        tmpdir = os.environ["TMPDIR"]
-
-    cmd = """\
-run-metadetect-on-slices \
-  --config=%s/metadetect-v8.yaml \
-  --output-path=./mdet_data \
-  --seed=%d \
-  --use-tmpdir \
-  --tmpdir=%s \
-  --log-level=INFO \
-  --n-jobs=%d \
-  --band-names=griz %s %s %s %s""" % (
-        cwd, seed, tmpdir, cpus, mfiles[0], mfiles[1], mfiles[2], mfiles[3],
+def _process_file(fname, tname, band):
+    ei = fitsio.read(fname, ext="epochs_info")
+    ii = fitsio.read(fname, ext="image_info")
+    ei = esutil.numpy_util.add_fields(
+        ei,
+        [
+            ("tilename", "U12"),
+            ("band", "U1"),
+            ("expnum", "i8"),
+            ("ccdnum", "i4"),
+            ("image_flags", "i4"),
+        ],
     )
-    subprocess.run(cmd, shell=True, check=True)
+    ei["tilename"] = tname
+    ei["band"] = band
 
-    fnames = glob.glob("./mdet_data/%s*" % tilename)
+    expnums, ccdnums = _extract_expnum_ccdnum(ii)
+    ei["expnum"] = expnums[ei["image_id"]]
+    ei["ccdnum"] = ccdnums[ei["image_id"]]
+    ei["image_flags"] = ii["image_flags"][ei["image_id"]]
+    assert np.all(ei["ccdnum"] <= 62)
+    assert np.array_equal(ii["image_id"], np.arange(len(ii)))
+
+    try:
+        os.remove(fname)
+    except Exception:
+        pass
+
+    return ei, ii
+
+
+def _process_tile(tname, uccds):
+    fnames = _download_tile(tname, os.getcwd())
     for fname in fnames:
-        pth1 = os.path.realpath(os.path.abspath(fname))
-        pth2 = os.path.realpath(os.path.abspath(
-            os.path.join(opth, os.path.basename(fname)))
+        band = os.path.basename(fname).split("_")[2]
+        tname = os.path.basename(fname).split("_")[0]
+        assert band in ["g", "r", "i", "z"]
+        ei, _ = _process_file(fname, tname, band)
+
+        ofname = os.path.join(
+            "epochs_info",
+            band,
+            os.path.basename(fname)[:-len("pizza-cutter-slices.fits.fz")]
+            + "epochs-info.fits",
         )
-        if pth1 != pth2:
-            subprocess.run(
-                "mv %s %s" % (pth1, pth2),
-                shell=True,
-                check=True,
+
+        fitsio.write(ofname, ei, clobber=True)
+        for i in range(len(ei)):
+            uccds.add((ei["expnum"][i], ei["ccdnum"][i], ei["band"][i]))
+
+
+def main():
+    tnames = np.unique(np.array([
+        fn.split("_")[0]
+        for fn in fitsio.read("fnames.fits")["FILENAME"]
+    ]))
+
+    if len(sys.argv) == 1:
+        tnames = tnames[:int(sys.argv[1])]
+
+    for band in "griz":
+        os.makedirs("./epochs_info/" + band, exist_ok=True)
+    os.system("mkdir -p data")
+
+    with BNLCondorParallel(verbose=0, mem=2, cpus=40) as exc:
+        jobs = []
+        for tilename in PBar(tnames, total=len(tnames), desc="making jobs"):
+            jobs.append(
+                joblib.delayed(_process_tile)(
+                    tilename, set(),
+                )
             )
 
-    for mfile in mfiles:
-        try:
-            os.remove(mfile)
-        except Exception:
-            pass
+        for res in PBar(exc(jobs), total=len(jobs), desc="processing tiles"):
+            try:
+                res.result()
+            except Exception as e:
+                print("ERROR: " + repr(e), flush=True)
 
 
-cwd = os.path.abspath(os.path.realpath(os.getcwd()))
-seed = 100
-os.system("mkdir -p ./mdet_data")
-os.system("chmod go-rw ./mdet_data")
-opth = os.path.abspath("./mdet_data")
-
-if len(sys.argv) == 1:
-    d = fitsio.read(
-        os.path.join(cwd, "fnames.fits"),
-        lower=True,
-    )
-    tnames = sorted(list(set([
-        d["filename"][i].split("_")[0]
-        for i in range(d.shape[0])
-    ])))
-    rng = np.random.RandomState(seed=seed)
-    seeds = rng.randint(low=1, high=2**29, size=len(tnames))
-    tmpdir = None
-    cpus = 4
-else:
-    tnames = [sys.argv[1]]
-    tmpdir = "/data/beckermr/tmp/" + tnames[0] + "_mdet"
-    os.system("mkdir -p " + tmpdir)
-    cpus = 1
-
-if len(tnames) == 1:
-    _run_tile(tnames[0], seed, opth, tmpdir, cwd, cpus)
-else:
-    d = fitsio.read(
-        os.path.join(cwd, "fnames.fits"),
-        lower=True,
-    )
-    all_tnames = np.array([
-        d["filename"][i].split("_")[0]
-        for i in range(d.shape[0])
-    ])
-    for mem in [8]:
-        with BNLCondorParallel(verbose=0, mem=mem, cpus=cpus) as exc:
-            jobs = []
-            for tilename, seed in PBar(
-                zip(tnames, seeds), total=len(tnames), desc="making jobs"
-            ):
-                if (
-                    np.sum(all_tnames == tilename) == 4
-                    and len(
-                        glob.glob("%s/%s*.fits.fz" % (opth, tilename))
-                    ) == 0
-                ):
-                    jobs.append(
-                        joblib.delayed(_run_tile)(
-                            tilename, seed, opth, tmpdir, cwd, cpus,
-                        )
-                    )
-
-            for res in PBar(exc(jobs), total=len(jobs), desc="running mdet"):
-                try:
-                    res.result()
-                except Exception as e:
-                    print("ERROR: " + repr(e), flush=True)
+if __name__ == "__main__":
+    main()
