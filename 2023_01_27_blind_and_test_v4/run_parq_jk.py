404e64eb025606f037e87818e4ee4c55438eafc5
beckermr <becker.mr@gmail.com>
2023-02-05 16:00:54 -0600

    debug

--- 2023_01_27_blind_and_test_v4/run_parq_jk.py
+++ 2023_01_27_blind_and_test_v4/run_parq_jk.py
@@ -4,8 +4,6 @@ from des_y6utils.mdet import make_mdet_cuts
 from esutil.pbar import PBar
 import fastparquet
 import pandas as pd
-import numpy as np
-import joblib
 import gc
 from numpy.lib.recfunctions import repack_fields
 
@@ -67,37 +65,28 @@ def main():
 
     pq_fname = "metadetect_desdmv4_cutsv3_jk"
     first = True
-    num_done = 0
     num_obj = 0
 
-    loc = 0
-    chunk_size = 1
-    chunks = len(fnames) // chunk_size
-    chunks += 1
+    for fname in PBar(fnames):
 
-    with joblib.Parallel(n_jobs=chunk_size, verbose=0) as par:
-        for chunk in PBar(range(chunks)):
-            max_loc = min(loc + chunk_size, len(fnames))
-            _fnames = fnames[loc:max_loc]
-            jobs = [joblib.delayed(_read_and_mask)(fn) for fn in _fnames]
-            _d = np.concatenate(par(jobs), axis=0)
-            num_done += (max_loc-loc)
-            num_obj += len(_d)
-            _d = pd.DataFrame(_d)
-            fastparquet.write(
-                pq_fname, _d,
-                has_nulls=False,
-                write_index=False,
-                fixed_text={"mdet_step": len("noshear")},
-                compression="SNAPPY",
-                append=False if first else True,
-                row_group_offsets=10_000_000,
-                file_scheme="hive",
-                partition_on=["patch_num", "mdet_step"],
-            )
-            first = False
-            del _d
-            gc.collect()
+        _d = _read_and_mask(fname)
+        num_obj += len(_d)
+        _d = pd.DataFrame(_d)
+        print(_d["patch_num"][0], flush=True)
+        fastparquet.write(
+            pq_fname, _d,
+            has_nulls=False,
+            write_index=False,
+            fixed_text={"mdet_step": len("noshear")},
+            compression="SNAPPY",
+            append=False if first else True,
+            row_group_offsets=10_000_000,
+            file_scheme="hive",
+            partition_on=["patch_num", "mdet_step"],
+        )
+        first = False
+        del _d
+        gc.collect()
 
 
 if __name__ == "__main__":
