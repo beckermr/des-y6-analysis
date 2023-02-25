fec6a7e025234276d1b3b355ed31f907ba32eca1
beckermr <becker.mr@gmail.com>
2023-02-25 07:50:32 -0600

    ENH rerun with joint fit

--- 2023_02_25_run_mdet_nocoadd/run_tile_slac.py
+++ 2023_02_25_run_mdet_nocoadd/run_tile_slac.py
@@ -79,6 +79,7 @@ run-metadetect-on-slices \
 --tmpdir=%s \
 --log-level=INFO \
 --n-jobs=%d \
+--range=0:10 \
 --band-names=griz %s %s %s %s""" % (
             cwd, seed, tmpdir, cpus,
             mfiles[0], mfiles[1], mfiles[2], mfiles[3],
@@ -91,6 +92,7 @@ run-metadetect-on-slices \
 --seed=%d \
 --log-level=INFO \
 --n-jobs=%d \
+--range=0:10 \
 --band-names=griz %s %s %s %s""" % (
             cwd, seed, cpus,
             mfiles[0], mfiles[1], mfiles[2], mfiles[3],
