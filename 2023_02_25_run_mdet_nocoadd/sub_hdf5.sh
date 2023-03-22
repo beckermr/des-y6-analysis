e3462068c4e9f84eaffce515004c46027ee2389d
beckermr <becker.mr@gmail.com>
2023-03-22 06:26:41 -0500

    finalize code

--- 2023_02_25_run_mdet_nocoadd/sub_hdf5.sh
+++ 2023_02_25_run_mdet_nocoadd/sub_hdf5.sh
@@ -7,7 +7,7 @@ command: |
   source ~/.bashrc
   conda activate des-y6
   echo \`which python\`
-  python make_parquet_patches &> log_hdf5.oe
+  python make_parquet_patches.py &> log_hdf5.oe
   python make_hdf5.py >>log_hdf5.oe 2>&1
 " > wq_sub_hdf5.yaml
 
