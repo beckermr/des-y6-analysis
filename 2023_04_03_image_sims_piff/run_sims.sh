3f0fa9f963e69fbbd366bcedd390ae255cc0ab4a
Matthew R Becker <becker.mr@gmail.com>
2023-04-03 17:05:15 -0400

    ENH add scripts to run things

---
+++ 2023_04_03_image_sims_piff/run_sims.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export IMSIM_DATA=${MEDS_DIR}
+export TMPDIR=/data/beckermr/tmp
+
+mkdir -p ./sim_outputs_outpar
+run-eastlake-sim \
+  -v 1 \
+  --seed 234324 \
+  config.yaml \
+  ./sim_outputs_outpar \
+  output.nproc=-1
+  # --step_names pizza_cutter metadetect \
+  # --resume_from=./sim_outputs/job_record.pkl \
+
+  # --skip_completed_steps \
+  # --resume_from=./sim_outputs/job_record.pkl \
+
+#  --step_names src_extractor \
+#  --resume_from=./sim_outputs/job_record.pkl \
