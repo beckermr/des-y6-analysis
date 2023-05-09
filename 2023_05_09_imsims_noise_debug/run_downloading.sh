7e58f4b2138e5c98eeddd34a8b924df45313188f
beckermr <becker.mr@gmail.com>
2023-05-09 10:59:04 -0500

    download to right spot

--- 2023_05_09_imsims_noise_debug/run_downloading.sh
+++ 2023_05_09_imsims_noise_debug/run_downloading.sh
@@ -3,7 +3,7 @@
 for tname in DES0433-2332; do
   for band in r; do
     des-pizza-cutter-prep-tile \
-      --config des-pizza-slices-y6-v14.yaml \
+      --config des-pizza-slices-y6-v15-nostars.yaml \
       --tilename ${tname} \
       --band ${band}
   done
