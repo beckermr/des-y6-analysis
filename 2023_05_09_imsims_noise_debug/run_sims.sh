912ee3d9ac0f4604b170578cbf32e51e5bf879ed
beckermr <becker.mr@gmail.com>
2023-05-09 11:38:48 -0500

    run two

--- 2023_05_09_imsims_noise_debug/run_sims.sh
+++ 2023_05_09_imsims_noise_debug/run_sims.sh
@@ -3,12 +3,20 @@
 export IMSIM_DATA=${MEDS_DIR}
 export TMPDIR=/data/beckermr/tmp
 
-mkdir -p ./sim_outputs
+mkdir -p ./sim_outputs_plus
 run-eastlake-sim \
   -v 1 \
   --seed 234324 \
   eastlake-config.yaml \
-  ./sim_outputs $1
+  ./sim_outputs stamp.shear.g1=0.02 stamp.shear.g2=0.0
+
+mkdir -p ./sim_outputs_minus
+run-eastlake-sim \
+  -v 1 \
+  --seed 234324 \
+  eastlake-config.yaml \
+  ./sim_outputs stamp.shear.g1=-0.02 stamp.shear.g2=0.0
+
   # --step_names pizza_cutter metadetect \
   # --resume_from=./sim_outputs/job_record.pkl \
 
