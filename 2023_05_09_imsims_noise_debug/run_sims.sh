9f5a8eea9e6a74ca92a8a1729ddef1e2b4a7ec4e
beckermr <becker.mr@gmail.com>
2023-05-11 11:40:45 -0500

    ENH do sims with more than one mag

--- 2023_05_09_imsims_noise_debug/run_sims.sh
+++ 2023_05_09_imsims_noise_debug/run_sims.sh
@@ -3,19 +3,19 @@
 export IMSIM_DATA=${MEDS_DIR}
 export TMPDIR=/data/beckermr/tmp
 
-mkdir -p ./sim_outputs_plus
+mkdir -p ./sim_outputs_plus_$1
 run-eastlake-sim \
   -v 1 \
   --seed 234324 \
   eastlake-config.yaml \
-  ./sim_outputs_plus stamp.shear.g1=0.02 stamp.shear.g2=0.0
+  ./sim_outputs_plus_$1 stamp.shear.g1=0.02 stamp.shear.g2=0.0 gal.flux.fgmag=$1
 
-mkdir -p ./sim_outputs_minus
+mkdir -p ./sim_outputs_minus_$1
 run-eastlake-sim \
   -v 1 \
   --seed 234324 \
   eastlake-config.yaml \
-  ./sim_outputs_minus stamp.shear.g1=-0.02 stamp.shear.g2=0.0
+  ./sim_outputs_minus_$1 stamp.shear.g1=-0.02 stamp.shear.g2=0.0 gal.flux.fgmag=$1
 
   # --step_names pizza_cutter metadetect \
   # --resume_from=./sim_outputs/job_record.pkl \
