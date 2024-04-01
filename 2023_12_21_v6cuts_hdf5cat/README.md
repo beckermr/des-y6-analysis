68f7dde3d06ea4f44fe23d3a4fc43b1799e92af7
Matthew R Becker <becker.mr@gmail.com>
2024-04-01 17:53:49 -0400

    ENH latest code for making catalogs

---
+++ 2023_12_21_v6cuts_hdf5cat/README.md
@@ -0,0 +1,6 @@
+# steps / instructions
+
+1. use the des-hacking env at BNL
+2. run the script run01_*
+3. run the script run02_*
+4. run the notebook test_make_hdf5.ipynb
--- 2023_12_21_v6cuts_hdf5cat/run02_make_cuts.sh
+++ 2023_12_21_v6cuts_hdf5cat/run02_make_cuts.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 pizza-patches-make-cut-files \
-  --flist=`pwd`/mdet_flist.txt \
-  --uid-info=`pwd`/mdet_uids.yaml \
-  --patches=$(cat config.json | jq -r .patches) \
-  --outdir=`pwd`/mdet_data_v6cuts
+    --flist=`pwd`/mdet_flist.txt \
+    --uid-info=`pwd`/mdet_uids.yaml \
+    --patches=$(cat config.json | jq -r .patches) \
+    --outdir=`pwd`/mdet_data_v6cuts \
+    --clobber \
+    --parallel
 #    \
 #   --file-index=0
